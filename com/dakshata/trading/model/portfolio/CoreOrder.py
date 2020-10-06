c2e51380f1f1d38339c768a05dc160c9a969b405
PRITESH <mhatre.pritesh@gmail.com>
2020-10-06 07:40:48 +0530

    added missing time properties

--- com/dakshata/trading/model/portfolio/CoreOrder.py
+++ com/dakshata/trading/model/portfolio/CoreOrder.py
@@ -32,8 +32,8 @@ class CoreOrder:
         self.symbol = symbol
         self.independent_exchange = independentExchange
         self.independent_symbol = independentSymbol
-        self. = modifiedTime
-        self. = createdTime
+        self.modified_time = modifiedTime
+        self.modified_time = createdTime
 
     def __str__(self):
         return "Order[{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10}]".format( \
