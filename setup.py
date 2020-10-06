7252e74c98a14c13dd9bf3586682518065f2d5a2
PRITESH <mhatre.pritesh@gmail.com>
2020-10-06 20:20:35 +0530

    Added dependencies

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Pritesh-Mhatre/python-library",
     packages=setuptools.find_packages(),
+    install_requires=['requests'],    
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 4 - Beta",
