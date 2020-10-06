f98402e977e7d1105d40b819f284f121ccbbe8c6
PRITESH <mhatre.pritesh@gmail.com>
2020-10-06 19:09:41 +0530

    Added assertions and setup method

--- tests/test_AutoTrader.py
+++ tests/test_AutoTrader.py
@@ -7,67 +7,65 @@ class TestAutoTrader(unittest.TestCase):
     __PROD_SERVER = 'https://stocksdeveloper.in:9017/'
     
     __TEST_SERVER = 'http://localhost:9017'
+    
+    __API = None
 
+    @classmethod
+    def setUpClass(cls):
+        TestAutoTrader.__API = AutoTrader.create_instance('b25f5e2f-93cb-430e-a81d-f960a490034f', TestAutoTrader.__TEST_SERVER)
+        
     def test_place_regular_order(self):
         """
         Test placing a regular order.
         """
-        api = AutoTrader.create_instance('b25f5e2f-93cb-430e-a81d-f960a490034f', TestAutoTrader.__TEST_SERVER)
-        
-        result = api.place_regular_order('159401', 'NSE', 'WIPRO', 'BUY', 'LIMIT', 'INTRADAY', 1, 330.35, 0.0)
+        response = TestAutoTrader.__API.place_regular_order( \
+            '159401', 'NSE', 'WIPRO', 'BUY', 'LIMIT', 'INTRADAY', 1, 330.35, 0.0)
         
         # print(result)
         
-        self.assertTrue(result.success())
-        
-        self.assertIsNotNone(result.result)
-        
-        self.assertNotEqual('', result.result)
+        self.assertTrue(response.success())
+        self.assertIsNotNone(response.result)
+        self.assertNotEqual('', response.result)
 
     def test_place_bracket_order(self):
         """
         Test placing a bracket order.
         """
-        api = AutoTrader.create_instance('b25f5e2f-93cb-430e-a81d-f960a490034f', TestAutoTrader.__TEST_SERVER)
+        response = TestAutoTrader.__API.place_bracket_order( \
+            '159401', 'NSE', 'WIPRO', 'SELL', 'LIMIT', 1, 326.35, 0.0, 1, 1, 0)
         
-        result = api.place_bracket_order('159401', 'NSE', 'WIPRO', 'SELL', 'LIMIT', 1, 326.35, 0.0, 1, 1, 0)
+        # print(response)
         
-        # print(result)
-        
-        self.assertTrue(result.success())
-        
-        self.assertIsNotNone(result.result)
-        
-        self.assertNotEqual('', result.result)
-        
+        self.assertTrue(response.success())
+        self.assertIsNotNone(response.result)
+        self.assertNotEqual('', response.result)
 
     def test_place_cover_order(self):
         """
         Test placing a cover order.
         """
-        api = AutoTrader.create_instance('b25f5e2f-93cb-430e-a81d-f960a490034f', TestAutoTrader.__TEST_SERVER)
+        response = TestAutoTrader.__API.place_cover_order( \
+            '159401', 'NSE', 'SBIN', 'SELL', 'LIMIT', 1, 188.15, 190.0)
         
-        result = api.place_cover_order('159401', 'NSE', 'SBIN', 'SELL', 'LIMIT', 1, 188.15, 190.0)
+        # print(response)
         
-        # print(result)
-        
-        self.assertTrue(result.success())
-        
-        self.assertIsNotNone(result.result)
-        
-        self.assertNotEqual('', result.result)
+        self.assertTrue(response.success())        
+        self.assertIsNotNone(response.result)
+        self.assertNotEqual('', response.result)
         
     def test_read_platform_margins(self):
         """
         Test reading margins data.
-        """
-        api = AutoTrader.create_instance('b25f5e2f-93cb-430e-a81d-f960a490034f', TestAutoTrader.__TEST_SERVER)
+        """        
+        response = TestAutoTrader.__API.read_platform_margins('159401')
         
-        result = api.read_platform_margins('159401')
+        # print(response)
         
-        # print(result)
+        self.assertTrue(response.success())
+        self.assertIsNotNone(response.result)
+        self.assertIsInstance(response.result, list)        
         
-        print(*result.result, sep = "\n") 
+        # print(*response.result, sep = "\n\n")
     
 if __name__ == '__main__':
     unittest.main()
