c1436e98bcb80859d5c8cdb662d168659cdc5de1
PRITESH <mhatre.pritesh@gmail.com>
2020-10-06 10:49:01 +0530

    Test cases for BO & CO

--- tests/test_AutoTrader.py
+++ tests/test_AutoTrader.py
@@ -14,7 +14,42 @@ class TestAutoTrader(unittest.TestCase):
         """
         api = AutoTrader.create_instance('b25f5e2f-93cb-430e-a81d-f960a490034f', TestAutoTrader.__TEST_SERVER)
         
-        result = api.place_regular_order('159401', 'NSE', 'WIPRO', 'BUY', 'LIMIT', 'INTRADAY', 10, 330.35, 0.0)
+        result = api.place_regular_order('159401', 'NSE', 'WIPRO', 'BUY', 'LIMIT', 'INTRADAY', 1, 330.35, 0.0)
+        
+        # print(result)
+        
+        self.assertTrue(result.success())
+        
+        self.assertIsNotNone(result.result)
+        
+        self.assertNotEqual('', result.result)
+
+    def test_place_bracket_order(self):
+        """
+        Test placing a bracket order.
+        """
+        api = AutoTrader.create_instance('b25f5e2f-93cb-430e-a81d-f960a490034f', TestAutoTrader.__TEST_SERVER)
+        
+        result = api.place_bracket_order('159401', 'NSE', 'WIPRO', 'SELL', 'LIMIT', 1, 326.35, 0.0, 1, 1, 0)
+        
+        # print(result)
+        
+        self.assertTrue(result.success())
+        
+        self.assertIsNotNone(result.result)
+        
+        self.assertNotEqual('', result.result)
+        
+
+    def test_place_cover_order(self):
+        """
+        Test placing a cover order.
+        """
+        api = AutoTrader.create_instance('b25f5e2f-93cb-430e-a81d-f960a490034f', TestAutoTrader.__TEST_SERVER)
+        
+        result = api.place_cover_order('159401', 'NSE', 'SBIN', 'SELL', 'LIMIT', 1, 188.15, 190.0)
+        
+        print(result)
         
         self.assertTrue(result.success())
         
